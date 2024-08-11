bdd23f6c59e2559f30c571870e6f373c76a9cf07
NelsonSolano <rafasolano2011@gmail.com>
2024-08-10 21:49:30 -0300

    Modificacion del Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,32 +1,67 @@
 
 
-function Footer({ appName }) {
+// function Footer({ appName }) {
     
-    return (
-        <footer
-            className= "footer"
-        >
-            <div className="content has-text-centered">
-        {/* <p>Todos los derechos reservados</p> */}
+//     return (
+//         <footer
+//             className= "footer"
+//         >
+//             <div className="content has-text-centered">
+//         {/* <p>Todos los derechos reservados</p> */}
 
-        <a className='icon' href= "https://instagram.com">
-           <i className="fab fa-instagram"></i> 
+//         <a className='icon' href= "https://instagram.com">
+//            <i className="fab fa-instagram"></i> 
           
-        </a>
-        <a className='icon' href= "https://twitter.com">
-           <i className="fab fa-twitter"></i> 
+//         </a>
+//         <a className='icon' href= "https://twitter.com">
+//            <i className="fab fa-twitter"></i> 
          
-        </a>
-        <a className='icon' href= "https://facebook.com">
-          <i className="fab fa-facebook"></i> 
+//         </a>
+//         <a className='icon' href= "https://facebook.com">
+//           <i className="fab fa-facebook"></i> 
         
-        </a>
-        <p
-                               >
-                    &copy; 2024 {appName}. Todos los derechos reservados.
-                </p>
-        </div>
+//         </a>
+//         <p
+//                                >
+//                     &copy; 2024 {appName}. Todos los derechos reservados.
+//                 </p>
+//         </div>
+//       </footer>
+//     );
+//   }
+//   export default Footer;
+
+import React from 'react';
+import '../styles/footer.css';
+
+function Footer({ appName }) {
+  return (
+      <footer className="footer">
+          <div className="content has-text-centered">
+              <div className="social-icons">
+                  <a className="icon" href="https://instagram.com" aria-label="Instagram">
+                      <i className="fab fa-instagram"></i>
+                  </a>
+                  <a className="icon" href="https://twitter.com" aria-label="Twitter">
+                      <i className="fab fa-twitter"></i>
+                  </a>
+                  <a className="icon" href="https://facebook.com" aria-label="Facebook">
+                      <i className="fab fa-facebook"></i>
+                  </a>
+                  <a className="icon" href="https://wa.me/3875016929" aria-label="WhatsApp">
+                        <i className="fab fa-whatsapp"></i>
+                    </a>
+                  <a className="icon" href="https://www.youtube.com/playlist?list=PLexg68_-qlOc8mXKWNq_EclmyNtkgP4Nf" aria-label="YouTube">
+                        <i className="fab fa-youtube"></i>
+                    </a>
+              </div>
+              
+              <p className="footer-text">
+                  &copy; 2024 {appName}. Todos los derechos reservados.
+              </p>
+          </div>
       </footer>
-    );
-  }
-  export default Footer;
+  );
+}
+
+export default Footer;
